8ad50501ec5b93fd5f55efc6c9b6ffe6c3fbfe24
FETYagency <fetyagency@gmail.com>
2023-07-26 10:48:28 +0100

    paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/INTERMIDIATE_dicWords /",
+  base:"/INTERMIDIATE_dicWords/",
   plugins: [react()],
 })
